d8e7e2856e4dea681a880022dec3be70d0a48cd8
mspr <mspr9773@gmail.com>
2025-03-21 12:01:16 +0530

    All files added

---
+++ 6-firewalls.tf
@@ -0,0 +1,23 @@
+# resource "google_compute_firewall" "allow_iap_ssh" {
+#   name    = "allow-iap-ssh"
+#   network = google_compute_network.vpc.name
+
+#   allow {
+#     protocol = "tcp"
+#     ports    = ["22"]
+#   }
+
+#   source_ranges = ["35.235.240.0/20"]
+# }
+
+resource "google_compute_firewall" "allow_custom_ports" {
+  name    = "allow-custom-ports"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "9090", "9093"]
+  }
+
+  source_ranges = ["0.0.0.0/0"] # Allow from all IPs (Adjust for security)
+}
